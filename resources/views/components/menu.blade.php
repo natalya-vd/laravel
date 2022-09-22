91f3daff986a2888ca3815ae8ea3c5683ad8afac
Natalya <natasha-vd@yandex.ru>
2022-09-23 00:08:28 +0300

    add homework-1

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,13 @@
+<nav {{ $attributes }}>
+    <ul>
+        <li>
+            <a href="/">Главная</a>
+        </li>
+        <li>
+            <a href="/info">Инфо</a>
+        </li>
+        <li>
+            <a href="/news">Новости</a>
+        </li>
+    </ul>
+</nav>
